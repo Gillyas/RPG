692162ecf765e4e3a4404f1e34e81f9290457386
Gillyas <155540907+Gillyas@users.noreply.github.com>
2024-03-14 17:05:07 +0400

    Add files via upload

---
+++ RPG_DZ/src/Main.java
@@ -0,0 +1,11 @@
+import javax.imageio.IIOException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        }
+    }
---
+++ RPG_DZ/src/Merchant.java
@@ -0,0 +1,13 @@
+public class Merchant implements Seller {
+    public String sell(Goods goods){
+        String result = " ";
+        if(goods == Goods.POTION){
+            result = "potion";
+
+        }
+        return  result;
+    }
+    public enum Goods {
+        POTION
+    }
+}
